694cea7c1f7fed424acdce6417e7cab05b25d21e
rpressiani <riccardo.pressiani@mail.polimi.it>
2016-05-06 09:05:43 +0200

    Added player's SimpleItem and a subclass for each of them.

---
+++ src/main/java/player/Assistants.java
@@ -0,0 +1,18 @@
+package player;
+
+public class Assistants extends SimpleItem {
+	
+	
+	/**
+	 * Subtract n-items from the total
+	 * 
+	 * @param	items						number of items to subtract
+	 * @throws	IllegalArgumentException	if the argument is NOT greater than zero
+	 */
+	public void sub(Integer items){
+		if (items < 0) throw new IllegalArgumentException("Argument must be greater than zero");
+		this.setItems(Integer.sum(this.getItems(), -items));
+		
+	}
+
+}
---
+++ src/main/java/player/Score.java
@@ -0,0 +1,5 @@
+package player;
+
+public class Score extends SimpleItem {
+
+}
