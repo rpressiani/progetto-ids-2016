e9b8bc2f20445f06b0961ab27f1d87fb8d83312f
rpressiani <riccardo.pressiani@mail.polimi.it>
2016-05-07 21:14:13 +0200

    Council feature added.

--- src/main/java/council/Counsellor.java
+++ src/main/java/council/Counsellor.java
@@ -3,10 +3,27 @@
  */
 package council;
 
+import politicalDeck.Color;
+
 /**
  * @author Riccardo Pressiani
  *
  */
 public class Counsellor {
+	
+	private Color color;
+	
+	public Counsellor(Color color){
+		this.color = color;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+	
+	
 
 }
---
+++ src/main/java/council/CounsellorGroup.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package council;
+
+import politicalDeck.Color;
+
+/**
+ * @author Riccardo Pressiani
+ *
+ */
+public class CounsellorGroup {
+	
+	private Color color;
+	private Integer counter;
+	
+	public CounsellorGroup(Color color, Integer counter){
+		this.color = color;
+		this.counter = counter;
+	}
+	
+	public void add(){
+		this.counter++;
+	}
+	
+	public void remove(){
+		this.counter--;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	
+	/**
+	 * @return the counter
+	 */
+	public Integer getCounter() {
+		return counter;
+	}
+	
+	
+
+
+}
