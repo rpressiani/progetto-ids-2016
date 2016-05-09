c0d41a9dac98b0bbe49e8e13d8492ea6845de4ee
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-05-09 11:59:05 +0200

    added method for shortest path in Map.java. It returns a List.

--- src/main/java/map/Map.java
+++ src/main/java/map/Map.java
@@ -1,13 +1,20 @@
 package map;
-/*import org.jgraph.JGraph;
+import java.util.List;
+
+import org.jgraph.JGraph;
 import org.jgraph.graph.DefaultEdge;
 import org.jgraph.graph.DefaultGraphCell;
 import org.jgrapht.UndirectedGraph;
+import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.SimpleGraph;
 import org.jgrapht.graph.builder.UndirectedGraphBuilder;
 
 public class Map {
-	UndirectedGraph<City, DefaultEdge> map = new SimpleGraph<City, DefaultEdge>; 
+	UndirectedGraph<City, DefaultEdge> map; 
 	
+	public Map() {
+		UndirectedGraph<City, DefaultEdge> map = new SimpleGraph<City, DefaultEdge>(DefaultEdge.class);
+	}
 	public void addCity(City c) {
 		map.addVertex(c);
 	}
@@ -17,9 +24,15 @@ public class Map {
 	public UndirectedGraph<City, DefaultEdge> getGraph() {
         return map;
     }
+	//Dijkstra's algorithm, even if it's more of a BFS
+	public List<DefaultEdge> shortestPath(UndirectedGraph<City, DefaultEdge> map, City c1, City c2) {
+		if(map==null) {
+			return null; 
+		}
+		DijkstraShortestPath<City, DefaultEdge> pathFinder = new DijkstraShortestPath<City, DefaultEdge>(map, c1, c2);
+		List<DefaultEdge> path = pathFinder.getPathEdgeList();
+		return path;
+	}
 	
 	
-	
-	
-	
-}*/
+} 
