27fdcb887b4d87735c421372eb64e17384e7c95b
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-05-16 10:42:36 +0200

    added some Javadoc

--- src/main/java/market/Contract.java
+++ src/main/java/market/Contract.java
@@ -28,6 +28,9 @@ public class Contract {
 		this.seller = seller;
 	}
 	
+	/**
+	 * @param nCoins
+	 */
 	public void sellCoins(Integer nCoins){
 		Coins sellingCoins = new Coins(nCoins);
 		if (sellingCoins.verifyAdd(seller) == true) {
@@ -36,6 +39,9 @@ public class Contract {
 		else System.out.println(this.seller + "doesn't have enough coins!");
 	}
 	
+	/**
+	 * @param nAssistants
+	 */
 	public void sellAssistants(Integer nAssistants){
 		Assistants sellingAssistants = new Assistants(nAssistants);
 		if (sellingAssistants.verifyAdd(seller) == true) {
@@ -44,10 +50,16 @@ public class Contract {
 		else System.out.println(this.seller + "doesn't have enough coins!");
 	}
 	
+	/**
+	 * @param nCoins
+	 */
 	public void buyCoins(Integer nCoins){
 		buyBag.add(new Coins(nCoins));
 	}
 	
+	/**
+	 * @param nAssistants
+	 */
 	public void buyAssistants(Integer nAssistants){
 		buyBag.add(new Assistants(nAssistants));
 	}
