d08ce426d947d9a8d0a27dc5ae233e5e32a79d37
rpressiani <riccardo.pressiani@mail.polimi.it>
2016-05-21 12:42:42 +0200

    Add King constructor, jaxb refresh.

--- src/main/java/model/sharedObjects/King.java
+++ src/main/java/model/sharedObjects/King.java
@@ -1,10 +1,22 @@
 package model.sharedObjects;
 
 import model.map.City;
+import model.map.Map;
+import parser.Parser;
 
 public class King {
 	
 	private City kingCity;
+	
+	public King(Parser parser, Map map){
+		
+		for (City city : map.allVertexes()) {
+			if (city.getName() == parser.getCFGRoot().getMap().getKingInitLocation()) {
+				this.kingCity = city;
+				break;
+			}
+		}
+	}
 
 	public City getKingCity() {
 		return kingCity;
