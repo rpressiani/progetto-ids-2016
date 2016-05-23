568d2a50a8ad3f6376ddef14448e2e77b19226d3
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-05-23 20:48:52 +0200

    add FailChange

---
+++ src/main/java/controller/FailChange.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.actions.NullAction;
+
+public class FailChange extends Change {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7310486880687184380L;
+	private NullAction failedAction; 
+	
+	public FailChange(NullAction failedAction) {
+		this.failedAction = failedAction; 
+	}
+	public NullAction getFailedAction() {
+		return failedAction; 
+	}
+	@Override
+	public String toString() {
+		return "FailChange [failedAction=" + failedAction + "]";
+	}
+	
+
+}
