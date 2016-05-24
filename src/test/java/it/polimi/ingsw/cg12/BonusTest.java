9beabd3342baee487392765c7124e5ae686beb4a
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-05-24 11:24:28 +0200

    other skeletons

---
+++ src/test/java/it/polimi/ingsw/cg12/BonusTest.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.cg12;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BonusTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/it/polimi/ingsw/cg12/PlayerTest.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.cg12;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
