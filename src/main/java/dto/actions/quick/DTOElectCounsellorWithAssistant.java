13d76c9f3d7228f72dfb37b8530fd3f621c25dd9
rpressiani <riccardo.pressiani@mail.polimi.it>
2016-05-27 20:38:11 +0200

    Add DTO object for ElectCounsellorWithAssistant action

---
+++ src/main/java/dto/actions/quick/DTOElectCounsellorWithAssistant.java
@@ -0,0 +1,36 @@
+package dto.actions.quick;
+
+import dto.DTOObject;
+import dto.map.DTORegion;
+import dto.utilities.DTOColor;
+
+public class DTOElectCounsellorWithAssistant implements DTOObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1369578544438130132L;
+	
+	private final DTORegion region;
+	private final DTOColor color;
+	
+	public DTOElectCounsellorWithAssistant(DTORegion region, DTOColor color) {
+		this.region = region;
+		this.color = color;
+	}
+
+	/**
+	 * @return the region
+	 */
+	public DTORegion getRegion() {
+		return region;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public DTOColor getColor() {
+		return color;
+	}
+
+}
