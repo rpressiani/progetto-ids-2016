1de49e1e792b3e0c69cbc0ee42096c2cdb3ee4e9
rpressiani <riccardo.pressiani@mail.polimi.it>
2016-05-27 20:50:53 +0200

    Add DTO object for HireAssistant action

---
+++ src/main/java/dto/actions/quick/DTOHireAssistant.java
@@ -0,0 +1,12 @@
+package dto.actions.quick;
+
+import dto.DTOObject;
+
+public class DTOHireAssistant implements DTOObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -857615911131416134L;
+
+}
