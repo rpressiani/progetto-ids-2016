4d57b202ed2f16c26ca9bc19c2848c7662bbe66c
MatteoPenco <matteo.penco05gmail.com>
2016-06-10 11:59:08 +0200

    added translateAction in RMIView

--- src/main/java/view/RMIViewRemote.java
+++ src/main/java/view/RMIViewRemote.java
@@ -4,9 +4,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import client.ClientViewRemote;
+import dto.actions.DTOAction;
+import model.actions.GeneralAction;
 
 
 public interface RMIViewRemote extends Remote {
 
 	public void registerClient(ClientViewRemote clientStub) throws RemoteException;
+	public void translateAction(DTOAction action) throws RemoteException;
 }
