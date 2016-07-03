90616a1d3c779b0b404a82df24205f104352427a
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-07-03 20:17:24 +0200

    new scene

---
+++ src/main/java/gui/BuildWithCardController.java
@@ -0,0 +1,27 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class BuildWithCardController {
+
+	@FXML
+	private TextArea card; 
+	@FXML
+	private TextArea city;
+	@FXML
+	private Button ok; 
+	@FXML
+	private Button cancel; 
+	
+	@FXML
+	private void handleOk() {
+		
+	}
+	@FXML
+	private void handleCancel() {
+		
+	}
+	
+}
