cbc1e924385828d141c0273cbd558960a324435f
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-07-03 23:21:36 +0200

    scene ready

--- src/main/java/gui/SellActionController.java
+++ src/main/java/gui/SellActionController.java
@@ -1,7 +1,45 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class SellActionController {
 	
 	private MainApp mainApp; 
+	
+	@FXML
+	private Button ok; 
+	@FXML
+	private Button cancel; 
+	@FXML
+	private TextArea sellCoins; 
+	@FXML
+	private TextArea sellAssistants; 
+	@FXML
+	private TextArea sellPermissions; 
+	@FXML
+	private TextArea sellPoliticals; 
+	@FXML
+	private TextArea buyCoins; 
+	@FXML
+	private TextArea buyAssistants; 
+	@FXML
+	private TextArea buyPermissions; 
+	@FXML
+	private TextArea buyPoliticals; 
+	
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp; 
+	}
+	
+	@FXML
+	private void handleOk() {
+		
+	}
+	@FXML
+	private void handleCancel() {
+		
+	}
 
 }
