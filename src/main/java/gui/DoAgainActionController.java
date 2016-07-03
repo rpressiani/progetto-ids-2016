68232cb449f9c0e8f6206d399ffc2824a9107d57
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-07-04 00:01:42 +0200

    new scene in progress

---
+++ src/main/java/gui/DoAgainActionController.java
@@ -0,0 +1,36 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DoAgainActionController {
+	
+	private MainApp mainApp; 
+	
+	@FXML
+	private Button buildWithCard = new Button(); 
+	@FXML
+	private Button buildWithKing = new Button(); 
+	@FXML
+	private Button electCounsellor = new Button(); 
+	@FXML
+	private Button buyPermissionCard = new Button(); 
+	
+	@FXML
+	private void handleBuildWithCard() {
+		
+	}
+	@FXML
+	private void handleBuildWithKing() {
+		
+	}
+	@FXML
+	private void handleElectCounsellor() {
+		
+	}
+	@FXML
+	private void handleBuyPermissionCard() {
+		
+	}
+
+}
