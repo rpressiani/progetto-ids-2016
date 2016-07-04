a25ce466148db2823cc0a12db9d2a2e44a2a0e7f
alessandro_pezzotta <alessandro.pezzotta@polimi.it>
2016-07-04 11:28:46 +0200

    added 2 tests in TestGetFreeToken

--- src/test/java/it/polimi/ingsw/cg12/testModel/actions/inputBonus/TestGetFreeToken.java
+++ src/test/java/it/polimi/ingsw/cg12/testModel/actions/inputBonus/TestGetFreeToken.java
@@ -2,12 +2,19 @@ package it.polimi.ingsw.cg12.testModel.actions.inputBonus;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
 
+import model.GameState;
 import model.actions.inputBonus.GetFreeToken;
+import model.bonusItem.BonusToken;
 import model.map.City;
+import model.player.Player;
+import parser.Parser;
+import utilities.Color;
 
 public class TestGetFreeToken {
 
@@ -25,11 +32,38 @@ public class TestGetFreeToken {
 	}
 	@Test
 	public void testDoActionWorks() {
-		
+		Parser parser = new Parser(); 
+		Player player = new Player(); 
+		player.setNickname("Ale");
+		player.setColor(new Color("red"));
+		ArrayList<Player> players = new ArrayList<Player>(); 
+		players.add(player); 
+		GameState gameState = new GameState(parser, players); 
+		player.initPlayer(gameState.getPoliticalDeck(), 0, parser);
+		player.getBonusInputs().add(new BonusToken(2));  
+		City city = gameState.getMap().getAllCitiesHashMap().get("A"); 
+		Set<City> cities = new HashSet<City>(); 
+		cities.add(city); 
+		GetFreeToken bonus = new GetFreeToken(cities); 
+		bonus.doAction(player, gameState);
+		assertTrue(player.getBonusInputs().isEmpty()); 
 	}
 	@Test
 	public void testCheckConditionWorks() {
-		
+		Parser parser = new Parser(); 
+		Player player = new Player(); 
+		player.setNickname("Ale");
+		player.setColor(new Color("red"));
+		ArrayList<Player> players = new ArrayList<Player>(); 
+		players.add(player); 
+		GameState gameState = new GameState(parser, players); 
+		player.initPlayer(gameState.getPoliticalDeck(), 0, parser);
+		player.getBonusInputs().add(new BonusToken(2));  
+		City city = gameState.getMap().getAllCitiesHashMap().get("A"); 
+		Set<City> cities = new HashSet<City>(); 
+		cities.add(city); 
+		GetFreeToken bonus = new GetFreeToken(cities); 
+		assertTrue(!bonus.checkCondition(player, gameState)); 
 	}
 
 }
